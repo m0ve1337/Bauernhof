7cf7315a6546ef8396ec4bfd9b2f3829fe5d8aa0
m0ve1337 <lol@hispeed.ch>
2015-11-26 20:37:52 +0100

    Finally done with threads

--- GUI/src/threads/GUITread.java
+++ GUI/src/threads/GUITread.java
@@ -3,7 +3,6 @@ package threads;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Stack;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,12 +10,12 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 public class GUITread {
-	private JFrame			frame	= new JFrame("Test");
-	private JPanel			panel	= new JPanel();
-	private JProgressBar	bar		= new JProgressBar(0, 100);
-	private JLabel			label	= new JLabel("Threads: ");
-	private int				counter	= 0;
-	private Stack<Thread>	threads	= new Stack<>();
+	private JFrame			frame		= new JFrame("Test");
+	private JPanel			panel		= new JPanel();
+	private JProgressBar	bar			= new JProgressBar(0, 100);
+	private JLabel			label		= new JLabel("Threads waiting: ");
+	private int				counter		= 0;
+	private BarUpdater		updaterJob	= new BarUpdater();
 
 	public void createAndShowGUI() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,39 +23,15 @@ public class GUITread {
 		bar.setSize(200, 200);
 		panel.add(bar);
 		JButton button = new JButton("Knopf");
+
 		button.addActionListener(new ActionListener() {
 			@Override
-			public synchronized void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e) {
+				counter++;
+				setLabelText();
 
-				//				BarUpdater job = new BarUpdater();
-				//				Thread updater = new Thread(job);
-				//
-				//				threads.push(updater);
-				//				System.out.println("thread 2 Stack added");
-
-				System.out.println(counter);
-
-				new Thread() {
-					@Override
-					public void run() {
-						doSomething();
-					}
-
-					private synchronized void doSomething() {
-						for (int i = 0; i <= 100; i++) {
-							bar.setValue(i);
-
-							try {
-								Thread.sleep(50);
-
-							} catch (InterruptedException e) {
-								e.printStackTrace();
-							}
-							label.setText("Threads: " + counter);
-						}
-
-					}
-				}.start();
+				Thread updaterThread = new Thread(updaterJob);
+				updaterThread.start();
 
 			}
 		}
@@ -68,31 +43,37 @@ public class GUITread {
 		frame.setSize(200, 100);
 		frame.setVisible(true);
 	}
+
+	private void setLabelText() {
+		label.setText("Threads waiting: " + counter);
+
+	}
+
+	private class BarUpdater implements Runnable {
+
+		@Override
+		public void run() {
+
+			doSomething();
+		}
+
+		private synchronized void doSomething() {
+
+			for (int i = 0; i <= 100; i++) {
+				bar.setValue(i);
+
+				try {
+					Thread.sleep(50);
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
+			counter--;
+			setLabelText();
+
+		}
+
+	}
 }
-
-//	private class BarUpdater implements Runnable {
-//
-//		@Override
-//		public synchronized void run() {
-//
-//			System.out.println("threads size: " + threads.size());
-//
-//			doSomething();
-//
-//		}
-//		private void doSomething() {
-//			for (int i = 0; i <= 100; i++) {
-//				bar.setValue(i);
-//
-//				try {
-//					Thread.sleep(50);
-//
-//				} catch (InterruptedException e) {
-//					e.printStackTrace();
-//				}
-//				label.setText("Threads: " + counter);
-//			}
-//		}
-//
-//	}
-
